3026a464472a4fbbe41ece328c8117c6131a3833
shanti26b <prasanthi26b@gmail.com>
2017-07-08 23:48:43 +0530

    Add files via upload

---
+++ UsingObject.java
@@ -0,0 +1,22 @@
+package Object;
+//here we are using the object to show that by initializing the elements of the data we can change 
+
+public class UsingObject {//the value that we have initialize
+//here taking the main method that to execute
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Integer i = new Integer(2);//here we taking an object of integer data type has variable i
+	        setToThree(i);//and initialized as 2 and given that method to set had 3
+	        
+	        System.out.println(i);
+	        
+	    }//here it prints as 2
+
+	    public static void setToThree(Integer i) {
+	        i = new Integer(3);//it sets that the value and it is going to print 2
+	    }
+	}
+
+
